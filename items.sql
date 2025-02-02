002c7563b59158e0dfc57e0b7fb8ff932ba4b9a3
Artin <99654331+Artin-ir@users.noreply.github.com>
2025-02-02 17:45:28 +0330

    Create items.sql

---
+++ items.sql
@@ -0,0 +1,5 @@
+INSERT INTO `items`(`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('medikit','Medikit','20','0','1');
+INSERT INTO `items`(`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('bandage','Bandage','40','0','1');
+INSERT INTO `items`(`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('marijuana','Cannabis','250','0','1');
+INSERT INTO `items`(`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('meth','Methamphetamine','25','0','1');
+INSERT INTO `items`(`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('coca','Cocaine','150','0','1');
